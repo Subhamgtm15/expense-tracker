bf9961c0c784fe66bbe3cc4be171b6742e341670
Subham Gautam <gautamsubham65@gmail.com>
2025-12-11 02:31:41 +0545

    Add update/edit functionality and improve UI

--- client/src/components/main.jsx
+++ client/src/components/main.jsx
@@ -4,6 +4,7 @@ export default function Main() {
     const [amount, setAmount] = useState('10');
     const [description, setDescription] = useState('');
     const [type, setType] = useState('expense')
+    const [editingId, setEditingId] = useState(null);
 
     useEffect(() => {
         fetch('http://localhost:5000/api/transactions')
@@ -13,13 +14,26 @@ export default function Main() {
 
     async function submitTransaction(e) {
         e.preventDefault();
-        const res = await fetch('http://localhost:5000/api/transactions', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ amount: parseFloat(amount), type: type, description: description })
-        });
-        const transaction = await res.json();
-        setTransaction(prev => [...prev, transaction.transaction]);
+        if (!editingId) {
+            const res = await fetch('http://localhost:5000/api/transactions', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ amount: parseFloat(amount), type: type, description: description })
+            });
+            const transaction = await res.json();
+            setTransaction(prev => [...prev, transaction.transaction]);
+        } else {
+            const res = await fetch(`http://localhost:5000/api/transactions/${editingId}`, {
+                method: 'PUT',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ amount: parseFloat(amount), type: type, description: description })
+            });
+            const updatedTransaction = await res.json();
+            const updatedArray = transaction.map((t => t._id === editingId ? updatedTransaction.updated : t))
+            setTransaction(updatedArray)
+            setEditingId(null);
+        }
+
         setAmount('');
         setDescription('');
         setType('expense');
@@ -60,11 +74,25 @@ export default function Main() {
         return totalIn;
     }
 
-    return (
-        <main className="min-h-screen bg-gray-100 flex flex-col items-center p-6 gap-4">
-            <h1 className="font-bold text-2xl">Finance Tracker</h1>
-            <section className="grid grid-cols-1 sm:grid-cols-3 gap-4 w-full max-w-2xl mt-6">
+    //repeat form filling
+    useEffect(() => {
+        const refillTransaction = transaction.find(t => t._id === editingId);  //use find for single match, use filter for multiple matches
+        if (refillTransaction) {
+            setAmount(refillTransaction.amount);
+            setDescription(refillTransaction.description)
+            setType(refillTransaction.type)
+        }
+    }, [editingId])
 
+
+    return (
+        <main className=" bg-gray-100 flex flex-col items-center p-4 sm:p-6 gap-6">
+
+            <h1 className="font-bold text-2xl sm:text-3xl">Finance Tracker</h1>
+
+            {/* Summary Section */}
+            <section className="grid grid-cols-1 sm:grid-cols-3 gap-4 w-full max-w-4xl">
+                {/* Total Income */}
                 <div className="bg-white rounded-xl shadow p-4 flex flex-col border-l-4 border-green-500">
                     <p className="text-gray-500 text-sm font-medium">Total Income</p>
                     <h2 className="text-2xl font-bold text-green-600 mt-1">
@@ -72,6 +100,7 @@ export default function Main() {
                     </h2>
                 </div>
 
+                {/* Total Expenses */}
                 <div className="bg-white rounded-xl shadow p-4 flex flex-col border-l-4 border-red-500">
                     <p className="text-gray-500 text-sm font-medium">Total Expenses</p>
                     <h2 className="text-2xl font-bold text-red-600 mt-1">
@@ -79,53 +108,70 @@ export default function Main() {
                     </h2>
                 </div>
 
+                {/* Balance */}
                 <div className="bg-white rounded-xl shadow p-4 flex flex-col border-l-4 border-blue-500">
                     <p className="text-gray-500 text-sm font-medium">Balance</p>
-                    <h2 className={`text-2xl font-bold mt-1 ${totalIncome() - totalExpenses() >= 0
-                        ? "text-blue-600"
-                        : "text-red-600"
-                        }`}>
+                    <h2 className={`text-2xl font-bold mt-1 ${totalIncome() - totalExpenses() >= 0 ? "text-blue-600" : "text-red-600"}`}>
                         NPR {totalIncome() - totalExpenses()}
                     </h2>
                 </div>
-
             </section>
 
+            {/* Form + Transaction List Container */}
+            <div className="w-full max-w-4xl flex flex-col lg:flex-row gap-6">
 
-            <form onSubmit={submitTransaction} className="bg-white p-6 rounded-lg shadow-md w-full max-w-md space-y-4">
-                <h2 className="text-2xl font-bold text-gray-700 mb-4">Add Transaction</h2>
-                <input type="number" min="10" className="w-full p-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-400" placeholder="Enter Amount" value={amount} onChange={Amount} />
-                <select onChange={Type} value={type} className="w-full p-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-400">
-                    <option value='expense'>expense</option>
-                    <option value='income'>income</option>
-                </select>
-                <input type="text" className="w-full p-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-400" placeholder="description" value={description} onChange={Description} />
-                <button type='submit' className="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600 transition">Add</button>
-            </form>
+                {/* Transaction Form */}
+                <form onSubmit={submitTransaction} className="flex-1 bg-white p-6 rounded-lg shadow-md space-y-4">
+                    <h2 className="text-2xl font-bold text-gray-700 mb-4">Add Transaction</h2>
+                    <input type="number" min="10" className="w-full p-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-400" placeholder="Enter Amount" value={amount} onChange={Amount} />
+                    <select onChange={Type} value={type} className="w-full p-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-400">
+                        <option value='expense'>expense</option>
+                        <option value='income'>income</option>
+                    </select>
+                    <input type="text" className="w-full p-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-400" placeholder="Description" value={description} onChange={Description} />
+                    <button type='submit' className="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600 transition">{editingId ? "Update" : "Add"}</button>
+                </form>
 
-            {/* show transaction */}
-            <div className="mt-6 w-full max-w-md bg-white p-4 rounded-lg shadow-md">
-                <h2 className="text-lg font-semibold text-gray-700 mb-3">Transactions</h2>
-                {transaction.length === 0 ? (
-                    <p className="text-sm text-gray-500">No transactions yet.</p>
-                ) : (
-                    <div>
-                        {transaction.map((item, index) => (
-                            <div key={item._id} className="flex justify-between items-center py-2 border-b last:border-b-0">
-                                <div>
-                                    <p className="text-sm font-medium text-gray-800">{item.description}</p>
-                                    <p className="text-xs text-gray-500">{item.type.charAt(0).toUpperCase() + item.type.slice(1)}</p>
+                {/* Transaction List */}
+                <div className="flex-1 bg-white p-4 rounded-lg shadow-md max-h-[500px] ">
+                    <h2 className="text-lg font-semibold text-gray-700 mb-3">Transactions</h2>
+                    {transaction.length === 0 ? (
+                        <p className="text-sm text-gray-500">No transactions yet.</p>
+                    ) : (
+                        <div className="flex flex-col gap-2">
+                            {transaction.map(item => (
+                                <div key={item._id} className="flex justify-between items-center py-2 px-2 border-b last:border-b-0 rounded hover:bg-gray-50 transition">
+                                    <div>
+                                        <p className="text-sm font-medium text-gray-800">{item.description}</p>
+                                        <p className="text-xs text-gray-500">{item.type.charAt(0).toUpperCase() + item.type.slice(1)}</p>
+                                    </div>
+                                    <div className={`text-sm font-semibold ${item.type === 'income' ? 'text-green-600' : 'text-red-600'}`}>
+                                        NPR {item.amount}
+                                    </div>
+                                    <button className="px-3 py-1 bg-blue-500 hover:bg-blue-600 text-white font-medium rounded-lg transition-colors duration-200" onClick={() => {
+                                        if (editingId === item._id) {
+                                            setEditingId(null);
+                                            setAmount('');
+                                            setDescription('');
+                                            setType('expense');
+                                        } else {
+                                            setEditingId(item._id);
+                                        }
+                                    }}>{editingId===item._id ? 'cancel' : 'edit'}</button>
+                                    <button
+                                        className="px-3 py-1 bg-red-500 hover:bg-red-600 text-white font-medium rounded-lg transition-colors duration-200"
+                                        onClick={() => removeTransaction(item._id)}>
+                                        Remove
+                                    </button>
+
                                 </div>
-                                <div className={`text-sm font-semibold ${item.type === 'income' ? 'text-green-600' : 'text-red-600'}`}>
-                                    {item.amount}
-                                </div>
-                                <button className="px-3 py-1 bg-red-500 hover:bg-red-600 text-white font-medium rounded-lg transition-colors duration-200" onClick={() => removeTransaction(item._id)}>remove</button>
-                            </div>
-                        ))
-                        }
-                    </div>
-                )}
+                            ))}
+                        </div>
+                    )}
+                </div>
+
             </div>
         </main>
+
     )
 }
--- server/server.js
+++ server/server.js
@@ -32,33 +32,57 @@ app.get('/api/transactions', async (req, res) => {
 
 app.post('/api/transactions', async (req, res) => {
     const { amount, type, description } = req.body;
-    const newTransaction = { amount: amount, type: type, description: description, date:new Date()}
-    try{
-        const db=getDB();
-        const result=await db.collection('transactions').insertOne(newTransaction);
+    const newTransaction = { amount: amount, type: type, description: description, date: new Date() }
+    try {
+        const db = getDB();
+        const result = await db.collection('transactions').insertOne(newTransaction);
         res.status(201).json({
-            message:'successfully created',
+            message: 'successfully created',
             transaction: { ...newTransaction, _id: result.insertedId }
         })
-    } catch{
-        res.status(500).json({error:'failed to post'})
-    } 
+    } catch {
+        res.status(500).json({ error: 'failed to post' })
+    }
 })
 
 //delete transactions
-app.delete('/api/transactions/:id',async(req,res)=>{
-    const id=req.params.id
-    try{
-        const db=getDB();
-        const result=await db.collection('transactions').deleteOne({_id:new ObjectId(id)})
-        if(result.deletedCount===0){
-            return res.status(404).json({error:'transaction not found'})
+app.delete('/api/transactions/:id', async (req, res) => {
+    const id = req.params.id
+    try {
+        const db = getDB();
+        const result = await db.collection('transactions').deleteOne({ _id: new ObjectId(id) })
+        if (result.deletedCount === 0) {
+            return res.status(404).json({ error: 'transaction not found' })
         }
-        res.status(200).json({message:'successfully deleted',id})
-    }catch{
-        res.status(500).json({error:'failed to delete'})
+        res.status(200).json({ message: 'successfully deleted', id })
+    } catch {
+        res.status(500).json({ error: 'failed to delete' })
     }
 })
+
+//update transactions
+app.put('/api/transactions/:id', async (req, res) => {
+    const id = req.params.id
+    const { amount, type, description } = req.body;
+    try {
+        const db = getDB();
+        const result = await db.collection('transactions').updateOne(
+            { _id: new ObjectId(id) },
+            { $set: { amount: amount, type: type, description: description, } }
+        );
+        if (result.matchedCount === 0) {
+            return res.status(404).json({ error: 'Transaction not found' });
+        }
+
+        return res.status(200).json({
+            message: 'Transaction updated successfully',
+            updated: { id, amount, type, description }
+        });
+    } catch {
+        res.status(500).json({ error: 'failed to update' })
+    }
+})
+
 
 const PORT = 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
